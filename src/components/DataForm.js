951c6ad3399d34ba30909a33fc7ca76f4db586da
nmegrant <nme.grant@gmail.com>
2020-07-16 21:50:01 +0200

    Set up controlled form

--- src/components/DataForm.js
+++ src/components/DataForm.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default class DataForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userInput: "" };
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  handleUserInput(event) {
+    this.setState({ userInput: event.target.value });
+  }
+
   render() {
     return (
       <Form style={{ marginTop: "30px" }}>
@@ -10,8 +21,15 @@ export default class DataForm extends React.Component {
           <Form.Label style={{ marginRight: "10px" }}>
             What kind of gif do you want?
           </Form.Label>
-          <Form.Control type="text" />
+          <Form.Control
+            type="text"
+            onChange={this.handleUserInput}
+            value={this.state.userInput}
+          />
         </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
       </Form>
     );
   }
